b0ac93900d2bd3bd181754a29c897813f0304783
Aiswaryeaahh <aiswaryavs420@gmail.com>
2026-06-03 19:51:03 +0530

    intial commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://vardtxtmlbbeqckgbbmz.supabase.co';
+  static const supabaseAnonkey =
+      'sb_publishable_agPMx7EC3B7vIA53EAm6jg_-vmGZ2-j';
+}
---
+++ lib/feature/auth/presentation/pages/login_page.dart
@@ -0,0 +1,96 @@
+import 'package:blog_app/core/theme/app_palette.dart';
+import 'package:blog_app/feature/auth/presentation/pages/signup_page.dart';
+import 'package:blog_app/feature/auth/presentation/widgets/auth_field.dart';
+import 'package:blog_app/feature/auth/presentation/widgets/auth_gradient_button.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static Route route() =>
+      MaterialPageRoute(builder: (context) => const LoginPage());
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  late TapGestureRecognizer _signUpRecognizer;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    _signUpRecognizer.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _signUpRecognizer = TapGestureRecognizer()
+      ..onTap = () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SignUpPage()),
+        );
+      };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign In',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              AuthField(hintText: 'Email', controller: emailController),
+              const SizedBox(height: 15),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20),
+              const AuthGradientButton(buttonText: "Sign In"),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, SignUpPage.route());
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Don\'t have an account? ',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: 'Sign Up',
+                        recognizer: _signUpRecognizer,
+                        style: Theme.of(context).textTheme.titleMedium
+                            ?.copyWith(
+                              color: AppPallete.gradient2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
